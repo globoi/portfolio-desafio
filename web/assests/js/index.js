827865b5376824bc908af0ebbb90d6d2197709db
Bruno Prado <bwprado@gmail.com>
2023-12-08 12:27:14 -0300

    Add fetchNews function to fetch news from API

---
+++ web/assests/js/index.js
@@ -0,0 +1,15 @@
+//@ts-check
+
+/**
+ * @funtion fetchNews
+ * @description Fetches news from the API
+ * @param {string | number} page
+ * @returns {Promise<Article[]>}
+ */
+const fetchNews = async (page = 1) => {
+  const response = await fetch(`http://localhost:3000/feed/page/${page}`)
+  /** @type {Article[]} */
+  const news = await response.json()
+
+  return news
+}
