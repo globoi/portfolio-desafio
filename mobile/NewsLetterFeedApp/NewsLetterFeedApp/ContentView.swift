691485b2e7ead6c3f45e8ffee73ebe522bf2b608
matheushm <matheushm@ciandt.com>
2023-12-11 19:04:26 -0300

    config: create XCode project

---
+++ mobile/NewsLetterFeedApp/NewsLetterFeedApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NewsLetterFeedApp
+//
+//  Created by Matheus Henrique Mendes Alexandre on 11/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ mobile/NewsLetterFeedApp/NewsLetterFeedApp/NewsLetterFeedAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsLetterFeedAppApp.swift
+//  NewsLetterFeedApp
+//
+//  Created by Matheus Henrique Mendes Alexandre on 11/12/23.
+//
+
+import SwiftUI
+
+@main
+struct NewsLetterFeedAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
